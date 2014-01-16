1229d6feac3c0d028f60b1f46511fe25d00195dc
Ramin <rsedighi@gmail.com>
2014-01-15 22:45:37 -0800

    added turn gem and finalized MiniTest

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,51 +1,69 @@
 class ProductsController < ApplicationController
-  before_action :set_product, only: [:show, :edit, :update, :destroy]
-
   # GET /products
   # GET /products.json
   def index
     @products = Product.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @products }
+    end
   end
 
   # GET /products/1
   # GET /products/1.json
   def show
+    @product = Product.find(params[:id])
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.json { render json: @product }
+    end
   end
 
   # GET /products/new
+  # GET /products/new.json
   def new
     @product = Product.new
+
+    respond_to do |format|
+      format.html # new.html.erb
+      format.json { render json: @product }
+    end
   end
 
   # GET /products/1/edit
   def edit
+    @product = Product.find(params[:id])
   end
 
   # POST /products
   # POST /products.json
   def create
-    @product = Product.new(product_params)
+    @product = Product.new(params[:product])
 
     respond_to do |format|
       if @product.save
         format.html { redirect_to @product, notice: 'Product was successfully created.' }
-        format.json { render action: 'show', status: :created, location: @product }
+        format.json { render json: @product, status: :created, location: @product }
       else
-        format.html { render action: 'new' }
+        format.html { render action: "new" }
         format.json { render json: @product.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # PATCH/PUT /products/1
-  # PATCH/PUT /products/1.json
+  # PUT /products/1
+  # PUT /products/1.json
   def update
+    @product = Product.find(params[:id])
+
     respond_to do |format|
-      if @product.update(product_params)
+      if @product.update_attributes(params[:product])
         format.html { redirect_to @product, notice: 'Product was successfully updated.' }
         format.json { head :no_content }
       else
-        format.html { render action: 'edit' }
+        format.html { render action: "edit" }
         format.json { render json: @product.errors, status: :unprocessable_entity }
       end
     end
@@ -54,21 +72,12 @@ class ProductsController < ApplicationController
   # DELETE /products/1
   # DELETE /products/1.json
   def destroy
+    @product = Product.find(params[:id])
     @product.destroy
+
     respond_to do |format|
       format.html { redirect_to products_url }
       format.json { head :no_content }
     end
   end
-
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_product
-      @product = Product.find(params[:id])
-    end
-
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def product_params
-      params.require(:product).permit(:name, :price, :released_on)
-    end
 end
